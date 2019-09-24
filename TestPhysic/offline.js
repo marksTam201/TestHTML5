5c1bfceeb40656f86ea2a89bae8610119c535282
markstam <tamnt@beetsoft.com.vn>
2019-09-24 17:18:09 +0700

    Create project

---
+++ TestPhysic/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1569317743,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/body-sheet0.png",
+		"images/head-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
